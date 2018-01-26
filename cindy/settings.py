60790b3059229e5d268c17f3859b72e888403d3c
heyrict <xiezh0831@126.com>
2018-01-26 17:00:34 +0800

    Add hosts to security.py

--- cindy/settings.py
+++ cindy/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 from django.utils.translation import ugettext_lazy as _
-from .security import SECRET_KEY, POSTGREDB_SETTINGS
+from .security import SECRET_KEY, POSTGREDB_SETTINGS, HOSTS
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,7 +28,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     'cindythink.com',
-]
+] + HOSTS
 
 # Application definition
 
