aa391acc44af3f16e6fb314ac0d5f02d4aae0bb6
heyrict <xiezh0831@126.com>
2018-03-11 09:24:19 +0800

    Wrap routing with static file wrapper

--- sui_hei/routing.py
+++ sui_hei/routing.py
@@ -1,14 +1,16 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.staticfiles import StaticFilesWrapper
 from django.conf.urls import url
 
 from .consumers import GraphqlSubcriptionConsumer, MainConsumer
 
-application = ProtocolTypeRouter({
-    "websocket":
-    AuthMiddlewareStack(
-        URLRouter([
-            url("^ws/$", GraphqlSubcriptionConsumer),
-            url("^direct/$", MainConsumer),
-        ])),
-})
+application = StaticFilesWrapper(
+    ProtocolTypeRouter({
+        "websocket":
+        AuthMiddlewareStack(
+            URLRouter([
+                url("^ws/$", GraphqlSubcriptionConsumer),
+                url("^direct/$", MainConsumer),
+            ])),
+    }))
