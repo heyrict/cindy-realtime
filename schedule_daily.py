c92431f51a2c7c5bd4b17f3d6defb40bc69d4088
heyrict <xiezh0831@126.com>
2018-05-02 14:23:51 +0800

    Separating settings from schedule

--- schedule_daily.py
+++ schedule_daily.py
@@ -1,16 +1,19 @@
 '''isort:skip_file'''
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cindy.settings")
+import logging
+import yaml
 from datetime import timedelta
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cindy.settings")
 
 import django
 django.setup()
 from django.utils import timezone
 
 from awards import judgers, granters
-from sui_hei.models import Award, ChatMessage, ChatRoom, Puzzle, User
+from sui_hei.models import Award, ChatMessage, ChatRoom, Puzzle, User, DirectMessage
 
-daily_message = ""
+logger = logging.getLogger(name=__file__)
+logging.basicConfig(datefmt="%Y/%m/%d %H:%M:%S", level=logging.DEBUG)
 
 
 def feedBot(message):
@@ -23,48 +26,72 @@ def feedBot(message):
             ChatMessage(chatroom=lobby, user=user, content=message).save()
 
     except Exception as e:
-        print(e)
+        logger.debug(e)
 
 
-def clean_recent_minichat(recent=None):
+def clean_recent_minichat(chatroomName="lobby", recent=None):
+    cr = ChatRoom.objects.get(name=chatroomName)
+    cr_messages = cr.chatmessage_set.order_by("id")
+    count = cr_messages.count()
+
+    logger.debug("[ChatRoom:%s]: Total count: %s" % (chatroomName, count))
     if not isinstance(recent, int):
-        return
+        logger.debug("[ChatRoom:%s]: Delete all objects" % chatroomName)
+        cr.chatmessage_set.delete()
+    else:
+        logger.debug("[ChatRoom:%s]: Leaving message count: %s" % (chatroomName, recent))
 
-    lobby = ChatRoom.objects.get(name="lobby")
-    lobby_messages = ChatMessage.objects.filter(chatroom=lobby).order_by("id")
-    count = lobby_messages.count()
     if count < recent:
         return
 
     try:
-        earliest = lobby_messages[count - recent].id
+        earliest = cr_messages[count - recent].id
     except IndexError:
         return
 
-    lobby_messages.filter(id__lt=earliest).delete()
+    to_delete = cr_messages.filter(id__lt=earliest)
+    logger.debug("[ChatRoom:%s]: Deleting %s objects" % (chatroomName, to_delete.count()))
+    to_delete.delete()
+
+
+def clean_recent_directmessages(recent=90):
+    now = timezone.now()
+    recent_days_ago = now - timedelta(days=recent)
+    outdated = DirectMessage.objects.filter(created__lt=recent_days_ago)
+    logger.debug("[DirectMessage]: Reserve messages in recent %d days" % recent)
+    logger.debug("[DirectMessage]: Deleting %s objects" % outdated.count())
+    outdated.delete()
 
 
 def mark_puzzle_as_dazed(recent=14):
-    message = ""
-
     now = timezone.now()
     recent_days_ago = now - timedelta(days=recent)
     unsolved = Puzzle.objects.filter(status=0, modified__lt=recent_days_ago)
     for dazed_puzzle in unsolved:
-        print("Mark dazed: ", dazed_puzzle.id, "-", dazed_puzzle.title)
-        message += "%s - %s\n" % (dazed_puzzle.id, dazed_puzzle.title)
+        logger.debug("[Puzzle]: Mark dazed:", dazed_puzzle.id, "-", dazed_puzzle.title)
         dazed_puzzle.status = 2
         dazed_puzzle.modified = now
         dazed_puzzle.save()
 
-    return message
-
 
 if __name__ == "__main__":
+    settings = {}
+    if os.path.exists('./schedule_settings.yml'):
+        with open("./schedule_settings.yml") as f:
+            settings = yaml.load(f)
+
     # delete old minichat chat messages
-    clean_recent_minichat(200)
+    for chatroomName, recent in settings.get(
+            'chatroom_messages_preserve_count', {}).items():
+        clean_recent_minichat(chatroomName, recent)
+
+    # delete old direct messages
+    recent = settings.get('direct_message_preserve_days', None)
+    if isinstance(recent, int):
+        clean_recent_directmessages(recent)
 
     # mark outdated puzzles as dazed
+    #daily_message = ""
     #returned = mark_puzzle_as_dazed(14)
     #if returned:
     #    daily_message += "## Dazed Soup :coffee:\n" + '- ' + '\n- '.join(
