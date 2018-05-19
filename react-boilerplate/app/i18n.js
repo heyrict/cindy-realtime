55a1ef51e9c2e0f872ae366d5667ce9ee89ef6eb
heyrict <xiezh0831@yahoo.co.jp>
2018-05-19 20:14:37 +0800

    Remove french translation

--- react-boilerplate/app/i18n.js
+++ react-boilerplate/app/i18n.js
@@ -7,22 +7,18 @@
 import { addLocaleData } from 'react-intl';
 import enLocaleData from 'react-intl/locale-data/en';
 import jaLocaleData from 'react-intl/locale-data/ja';
-import frLocaleData from 'react-intl/locale-data/fr';
 
 import { DEFAULT_LOCALE } from './containers/App/constants'; // eslint-disable-line
 import enTranslationMessages from './translations/en.json';
 import jaTranslationMessages from './translations/ja.json';
-import frTranslationMessages from './translations/fr.json';
 
 export const appLocales = [
   'en',
   'ja',
-  'fr',
 ];
 
 addLocaleData(enLocaleData);
 addLocaleData(jaLocaleData);
-addLocaleData(frLocaleData);
 
 export const formatTranslationMessages = (locale, messages) => {
   const defaultFormattedMessages = locale !== DEFAULT_LOCALE
@@ -40,5 +36,4 @@ export const formatTranslationMessages = (locale, messages) => {
 export const translationMessages = {
   en: formatTranslationMessages('en', enTranslationMessages),
   ja: formatTranslationMessages('ja', jaTranslationMessages),
-  fr: formatTranslationMessages('fr', frTranslationMessages),
 };
