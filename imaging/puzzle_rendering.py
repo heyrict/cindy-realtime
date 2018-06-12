9d8cf7008e0a71d69e6b7659b987b36af422e507
heyrict <xiezh0831@126.com>
2018-06-12 10:16:16 +0800

    Drawing with canvas

--- imaging/puzzle_rendering.py
+++ imaging/puzzle_rendering.py
@@ -5,42 +5,90 @@ import markdown
 from bs4 import BeautifulSoup
 from PIL import Image, ImageDraw, ImageFont
 
+from .wcstring import wcstr
+
 BASE_DIR = os.path.split(os.path.abspath(__file__))[0]
-FONT_PATH = os.path.join(BASE_DIR, "NotoSansCJK-Bold.ttc")
-BG_PATH = os.path.join(BASE_DIR, "twitter_puzzle_bg.png")
+FONT_PATH = "NotoSansCJK-Bold.ttc"
+CANVAS_WIDTH = 600
 TITLE_FONTSIZE = 18
-TITLE_SPLIT = 12
+TITLE_SPLIT = 63
 CONTENT_FONTSIZE = 16
-CONTENT_MAXLINENUM = 14
-CONTENT_SPLIT = 15
+CONTENT_SPLIT = 72
 LINE_HEIGHT = 25
 OUTPUT_IMAGE_NAME = '/tmp/puzzle_render_output.png'
+APPENDS = 'ウミガメのスープ出題サイトCindy：www.cindythink.com'
+APPENDS_INDENT = 200
+APPENDS_FONTSIZE = 14
 
 
 def _split_lines(text, stop):
-    text = re.split(r'\n+', text)
-    text = [[part[i:i + stop] for i in range(0, len(part), stop)]
-            for part in text if part]
-    return sum(text, [])
+    output = []
+    text = re.split(r'[\r\n]+', text)
+    for part in text:
+        if not part:
+            continue
+        part = wcstr(part)
+        for i in range(0, len(part), stop):
+            output.append(part[i:i + stop])
+
+    return output
 
 
-def render(title, content):
-    img = Image.open(BG_PATH)
+def render(title,
+           content,
+           appends=APPENDS,
+           canvas_width=CANVAS_WIDTH,
+           font_path=FONT_PATH,
+           title_fontsize=TITLE_FONTSIZE,
+           title_split=TITLE_SPLIT,
+           content_fontsize=CONTENT_FONTSIZE,
+           content_split=CONTENT_SPLIT,
+           appends_indent=APPENDS_INDENT,
+           appends_fontsize=APPENDS_FONTSIZE,
+           line_height=LINE_HEIGHT,
+           output_image_name=OUTPUT_IMAGE_NAME):
+    title = _split_lines(title, title_split)
+    content = _split_lines(content, content_split)
+    hTitle = len(title) * line_height + line_height // 2
+    hContent = len(content) * line_height + line_height // 2
+    hAppends = line_height
+    img = Image.new('RGB', (canvas_width, hTitle + hContent + hAppends), "#fcf4dc")
     draw = ImageDraw.Draw(img)
-    title_font = ImageFont.truetype(FONT_PATH, TITLE_FONTSIZE)
-    content_font = ImageFont.truetype(FONT_PATH, CONTENT_FONTSIZE)
+    title_font = ImageFont.truetype(font_path, title_fontsize)
+    content_font = ImageFont.truetype(font_path, content_fontsize)
+    appends_font = ImageFont.truetype(font_path, appends_fontsize)
 
-    draw.text((0, 5), title, (0, 0, 0), font=title_font)
-    for i, txt in enumerate(_split_lines(content, CONTENT_SPLIT)):
-        if i >= CONTENT_MAXLINENUM:
-            break
-        draw.text((0, i * LINE_HEIGHT + 55), txt, (0, 0, 0), font=content_font)
+    # Drawing border
+    draw.rectangle((0, 0, 2, hTitle + hContent), fill="#c6aa4b")
+    draw.rectangle(
+        (canvas_width - 3, 0, canvas_width - 1, hTitle + hContent),
+        fill="#c6aa4b")
 
-    img.save(OUTPUT_IMAGE_NAME)
-    return OUTPUT_IMAGE_NAME
+    # Drawing Title background
+    draw.rectangle((0, 0, canvas_width, hTitle), fill="#c6aa4b")
+
+    for i, txt in enumerate(title):
+        draw.text(
+            (5, i * line_height + 5), txt, font=title_font, fill="#fcf4dc")
+
+    for i, txt in enumerate(content):
+        draw.text(
+            (5, i * line_height + hTitle + 5),
+            txt,
+            font=content_font,
+            fill="#c6aa4b")
+
+    draw.text((appends_indent, hTitle + hContent + 5), appends, font=appends_font, fill="#888")
+
+    img.save(output_image_name)
+    return output_image_name
 
 
 def textify(md):
-    html = markdown.markdown(md)
+    html = markdown.markdown(md, [
+        'markdown.extensions.extra',
+        'markdown.extensions.nl2br',
+        'markdown.extensions.tables'
+    ]) # yapf: disable
     soup = BeautifulSoup(html, 'html.parser')
     return soup.get_text()
--- sui_hei/signals.py
+++ sui_hei/signals.py
@@ -5,7 +5,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from twitter import OAuth, Twitter
 
-from imaging.puzzle_rendering import render, textify
 from sui_hei.models import Puzzle
 
 ENABLE_TWITTERBOT = settings.ENABLE_TWITTERBOT
@@ -14,6 +13,7 @@ TOKEN_SECRET = settings.TOKEN_SECRET
 CONSUMER_KEY = settings.CONSUMER_KEY
 CONSUMER_SECRET = settings.CONSUMER_SECRET
 TWEET_MESSAGE = settings.TWEET_MESSAGE
+TWEET_WITH_PICTURE = settings.TWEET_WITH_PICTURE
 
 logger = logging.Logger(__name__)
 
@@ -25,19 +25,24 @@ def add_twitter_on_puzzle_created(sender, instance, created, **kwargs):
             auth = OAuth(TOKEN, TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
             t = Twitter(auth=auth)
 
-            imgpath = render(instance.title, textify(instance.content))
-            with open(imgpath, 'rb') as f:
-                imgdata = f.read()
-
             params = {
                 'status': TWEET_MESSAGE % {
                     'user_nickname': instance.user.nickname,
                     'title': instance.title,
                     'id': instance.id
                 },
-                'media[]': imgdata,
             }
 
-            t.statuses.update_with_media(**params)
+            if TWEET_WITH_PICTURE:
+                from imaging.puzzle_rendering import render, textify
+
+                imgpath = render(instance.title, textify(instance.content))
+                with open(imgpath, 'rb') as f:
+                    imgdata = f.read()
+                params['media[]'] = imgdata
+                t.statuses.update_with_media(**params)
+            else:
+                t.statuses.update(**params)
+
         except Exception as e:
             logger.warning("Error update twitter status: %s" % e)
