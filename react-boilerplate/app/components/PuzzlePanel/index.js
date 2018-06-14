ce377221788b6a7c5454b669c30e5c95e7732f5d
heyrict <xiezh0831@126.com>
2018-06-14 11:21:33 +0800

    Add Genre Icon for ja locale

--- react-boilerplate/app/components/PuzzlePanel/index.js
+++ react-boilerplate/app/components/PuzzlePanel/index.js
@@ -5,30 +5,25 @@
  */
 
 import React from 'react';
-import moment from 'moment';
 import PropTypes from 'prop-types';
-import { Box, Row, Divider, Flex } from 'rebass';
+import { Box, Row, Divider } from 'rebass';
 import { RoundedPanel } from 'style-store';
-import { FormattedMessage } from 'react-intl';
 import styled from 'styled-components';
+
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
+import { makeSelectLocale } from 'containers/LanguageProvider/selectors';
 
 import TitleLabel from 'components/TitleLabel';
 import UserLabel from 'components/UserLabel';
 import RewardingModal from 'components/RewardingModal/Loadable';
-import sortMessages from 'components/FilterableList/messages';
 
 import StarLabel from './StarLabel';
 import CommentLabel from './CommentLabel';
 import BookmarkLabel from './BookmarkLabel';
 import ProcessLabel from './ProcessLabel';
 import StatusLabel from './StatusLabel';
-
-const PuzzleDate = styled.span`
-  color: gray;
-  @media (max-width: 760px) {
-    font-size: 0.9em;
-  }
-`;
 
 const UserCol = styled(Box)`
   text-align: center;
@@ -50,7 +45,7 @@ export class PuzzlePanel extends React.Component {
     this.toggleRewardingPanel = (s) => this.setState({ rewardingShown: s });
   }
   render() {
-    const node = this.props.node;
+    const { node, locale } = this.props;
     return (
       <RoundedPanel my={10}>
         <Row mx={10} py={10}>
@@ -59,22 +54,14 @@ export class PuzzlePanel extends React.Component {
             {this.props.additional}
           </UserCol>
           <Box w={[3 / 4, 5 / 6]} px={10}>
-            <Flex flexWrap="wrap">
-              <Box>
-                <TitleLabel
-                  genre={node.genre}
-                  puzzleId={node.rowid}
-                  title={node.title}
-                  yami={node.yami}
-                />
-              </Box>
-              <Box ml="auto" style={{ alignSelf: 'center' }}>
-                <PuzzleDate>
-                  <FormattedMessage {...sortMessages.created} />:{' '}
-                  {moment(node.created).format('YYYY-MM-DD HH:mm')}
-                </PuzzleDate>
-              </Box>
-            </Flex>
+            <TitleLabel
+              genre={node.genre}
+              puzzleId={node.rowid}
+              title={node.title}
+              yami={node.yami}
+              locale={locale}
+              created={node.created}
+            />
             <Divider my={2} />
             <ProcessLabel qCount={node.quesCount} uaCount={node.uaquesCount} />
             <StatusLabel status={node.status} />
@@ -107,6 +94,17 @@ export class PuzzlePanel extends React.Component {
 PuzzlePanel.propTypes = {
   node: PropTypes.object.isRequired,
   additional: PropTypes.any,
+  locale: PropTypes.string.isRequired,
 };
 
-export default PuzzlePanel;
+PuzzlePanel.defaultProps = {
+  locale: 'ja',
+};
+
+const mapStateToProps = createStructuredSelector({
+  locale: makeSelectLocale(),
+});
+
+const withConnect = connect(mapStateToProps);
+
+export default compose(withConnect)(PuzzlePanel);
--- react-boilerplate/app/components/TitleLabel/index.js
+++ react-boilerplate/app/components/TitleLabel/index.js
@@ -8,9 +8,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import moment from 'moment';
+import { Flex, Box } from 'rebass';
+import { ImgMd as Img } from 'style-store';
 import { FormattedMessage } from 'react-intl';
 import { withLocale } from 'common';
 
+import classicJp from 'images/classicJp.svg';
+import twentyQuestionsJp from 'images/twentyQuestionsJp.svg';
+import littleAlbatJp from 'images/littleAlbatJp.svg';
+import othersJp from 'images/othersJp.svg';
+import yamiJp from 'images/yamiJp.svg';
+
+import sortMessages from 'components/FilterableList/messages';
 import messages from './messages';
 
 const PuzzleTitle = styled(Link)`
@@ -22,6 +32,13 @@ const PuzzleTitle = styled(Link)`
   }
   @media (max-width: 760px) {
     font-size: 1.15em;
+  }
+`;
+
+const PuzzleDate = styled.span`
+  color: gray;
+  @media (max-width: 760px) {
+    font-size: 0.9em;
   }
 `;
 
@@ -40,18 +57,70 @@ const TranslatedGenre = ({ genre }) => {
   }
 };
 
+const genreInfo = [
+  {
+    name: 'classic',
+    pictureJp: classicJp,
+  },
+  {
+    name: 'twentyQuestions',
+    pictureJp: twentyQuestionsJp,
+  },
+  {
+    name: 'littleAlbat',
+    pictureJp: littleAlbatJp,
+  },
+  {
+    name: 'others',
+    pictureJp: othersJp,
+  },
+];
+
 TranslatedGenre.propTypes = {
   genre: PropTypes.number.isRequired,
 };
 
 function TitleLabel(props) {
+  const { yami, locale, puzzleId, genre, title, created } = props;
+  if (locale === 'ja') {
+    return (
+      <Flex alignItems="center">
+        <Img src={genreInfo[genre].pictureJp} alt={genreInfo[genre].name} />
+        {yami && <Img src={yamiJp} alt="yami" />}
+        <Flex w={1} ml={1} flexWrap="wrap">
+          <Box>
+            <PuzzleTitle to={withLocale(`/puzzle/show/${puzzleId}`)}>
+              {title}
+            </PuzzleTitle>
+          </Box>
+          <Box ml="auto" style={{ alignSelf: 'center' }}>
+            <PuzzleDate>
+              <FormattedMessage {...sortMessages.created} />:{' '}
+              {moment(created).format('YYYY-MM-DD HH:mm')}
+            </PuzzleDate>
+          </Box>
+        </Flex>
+      </Flex>
+    );
+  }
+
   return (
-    <PuzzleTitle to={withLocale(`/puzzle/show/${props.puzzleId}`)}>
-      [{<TranslatedGenre genre={props.genre} />}
-      {props.yami ? ' x ' : null}
-      {props.yami ? <FormattedMessage {...messages.yami} /> : null}]
-      {props.title}
-    </PuzzleTitle>
+    <Flex flexWrap="wrap">
+      <Box>
+        <PuzzleTitle to={withLocale(`/puzzle/show/${puzzleId}`)}>
+          [{<FormattedMessage {...messages[genreInfo[genre].name]} />}
+          {yami ? ' x ' : null}
+          {yami ? <FormattedMessage {...messages.yami} /> : null}]
+          {title}
+        </PuzzleTitle>
+      </Box>
+      <Box ml="auto" style={{ alignSelf: 'center' }}>
+        <PuzzleDate>
+          <FormattedMessage {...sortMessages.created} />:{' '}
+          {moment(created).format('YYYY-MM-DD HH:mm')}
+        </PuzzleDate>
+      </Box>
+    </Flex>
   );
 }
 
@@ -60,6 +129,8 @@ TitleLabel.propTypes = {
   yami: PropTypes.bool.isRequired,
   puzzleId: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
+  locale: PropTypes.string.isRequired,
+  created: PropTypes.string.isRequired,
 };
 
 export default TitleLabel;
