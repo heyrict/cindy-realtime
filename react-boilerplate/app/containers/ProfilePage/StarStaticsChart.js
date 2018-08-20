0afd2930c5f33154b2d3449027370cf2e740fa0a
heyrict <xiezh0831@126.com>
2018-08-20 19:37:12 +0800

    Change BarChart to PieChart

--- react-boilerplate/app/containers/ProfilePage/StarStaticsChart.js
+++ react-boilerplate/app/containers/ProfilePage/StarStaticsChart.js
@@ -6,49 +6,133 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 import { intlShape, FormattedMessage } from 'react-intl';
-import {
-  Bar,
-  BarChart,
-  Legend,
-  ResponsiveContainer,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from 'recharts';
+import { Pie, PieChart, Sector, ResponsiveContainer } from 'recharts';
 import LoadingDots from 'components/LoadingDots';
 
 import messages from './messages';
 
-function StarStaticsChart(props, context) {
-  const _ = context.intl.formatMessage;
-  const labelFormatter = (count) => `★${count}`;
-  if (!props.truncValueGroups) {
-    return <LoadingDots py={50} size={8} />;
-  }
-  if (props.truncValueGroups.edges.length < 1) {
-    return <FormattedMessage {...messages.noStars} />;
-  }
+const renderActiveShape = (props) => {
+  const RADIAN = Math.PI / 180;
+  const {
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    startAngle,
+    endAngle,
+    fill,
+    payload,
+    percent,
+    value,
+    name,
+  } = props;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 10) * cos;
+  const sy = cy + (outerRadius + 10) * sin;
+  const mx = cx + (outerRadius + 30) * cos;
+  const my = cy + (outerRadius + 30) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  const ey = my;
+  const textAnchor = cos >= 0 ? 'start' : 'end';
+
   return (
-    <ResponsiveContainer width="95%" height={200}>
-      <BarChart data={props.truncValueGroups.edges}>
-        <Legend />
-        <Bar
-          dataKey="node.count"
-          name={_(messages.starStaticsCount)}
-          fill="#c6b571"
-        />
-        <Tooltip labelFormatter={labelFormatter} />
-        <XAxis
-          dataKey="node.value"
-          domain={[0.5, 5.5]}
-          ticks={[1, 2, 3, 4, 5]}
-          type="number"
-          tickFormatter={labelFormatter}
-        />
-        <YAxis dataKey="node.count" allowDecimals={false} />
-      </BarChart>
-    </ResponsiveContainer>
+    <g>
+      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+        ★{name}
+      </text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius + 10}
+        fill={fill}
+      />
+      <path
+        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+        stroke={fill}
+        fill="none"
+      />
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        textAnchor={textAnchor}
+        fill="#c67182"
+      >
+        {value}
+      </text>
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        dy={18}
+        textAnchor={textAnchor}
+        fill="#ccc"
+      >
+        {`(Rate ${(percent * 100).toFixed(2)}%)`}
+      </text>
+    </g>
   );
+};
+
+class StarStaticsChart extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: 0,
+    };
+    this.onPieEnter = (data, index) => {
+      this.setState({
+        activeIndex: index,
+      });
+    };
+  }
+
+  render() {
+    const _ = this.context.intl.formatMessage;
+    if (!this.props.truncValueGroups) {
+      return <LoadingDots py={50} size={8} />;
+    }
+    if (this.props.truncValueGroups.edges.length < 1) {
+      return <FormattedMessage {...messages.noStars} />;
+    }
+    const data = this.props.truncValueGroups.edges.map(
+      ({ node: { count, value } }) => ({ count, value }),
+    );
+    data.sort((e1, e2) => e1.value - e2.value);
+
+    return (
+      <ResponsiveContainer width="95%" height={200}>
+        <PieChart>
+          <Pie
+            data={data}
+            dataKey="count"
+            name={_(messages.starStaticsCount)}
+            nameKey="value"
+            innerRadius={30}
+            outerRadius={50}
+            activeShape={renderActiveShape}
+            activeIndex={this.state.activeIndex}
+            onMouseEnter={this.onPieEnter}
+            onClick={this.onPieEnter}
+            fill="#c67182"
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    );
+  }
 }
 
 StarStaticsChart.propTypes = {
